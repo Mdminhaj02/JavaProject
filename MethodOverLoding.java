fc5ccf48d4fabc675b2357e236a22e9baa61a283
Mdminhaj02 <Mdminhajulislam2016@gmail.com>
2018-11-20 22:08:38 +0600

    Come back

--- MethodOverLoding.java
+++ MethodOverLoding.java
@@ -1,3 +1,18 @@
 class OverLoding{
+	void add(int a,int b){
+	int x;
+	x=a+b;
+	System.out.println(c);
+}
+	void add(int a,int b,int c){
+	int x;
+	x=a+b+c;
+	System.out.println(x);
+}
+}
 
+class Main{
+	public static void main(String [] args){
+
+}
 }
