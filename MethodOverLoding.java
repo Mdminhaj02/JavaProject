3f9802ee23998074299bbba121d19100c6d2aafb
Mdminhaj02 <Mdminhajulislam2016@gmail.com>
2018-11-20 22:03:58 +0600

    return 1st commit

--- MethodOverLoding.java
+++ MethodOverLoding.java
@@ -1,18 +1,3 @@
 class OverLoding{
-	void add(int a,int b){
-	int x;
-	x=a+b;
-	System.out.println(c);
-}
-	void add(int a,int b,int c){
-	int x;
-	x=a+b+c;
-	System.out.println(x);
-}
-}
 
-class Main{
-	public static void main(String [] args){
-
-}
 }
