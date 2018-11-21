5db7449b04cda174ecf01ac1e5fc9f2a36559979
Mdminhaj02 <Mdminhajulislam2016@gmail.com>
2018-11-21 21:28:45 +0600

    add name

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class practice{
+public static void main(String [] args){
+    System.out.println("Hello.");
+    disPlay("Minhaj");
+    }
+    void string disPlay(string name){
+        System.out.println("Name : "+name);
+    }
+}
